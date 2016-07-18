4420feff88bbb3ebd2f7706e2d2d802eb2c72355
[a621275] Ibrahima khoule <ibrahima.khoule@atos.net>
2016-07-18 11:18:08 +0000

    begin autobid

---
+++ app/controllers/autobids_controller.rb
@@ -0,0 +1,5 @@
+class AutoBidsController < ApplicationController
+
+    def create
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625165050) do
+ActiveRecord::Schema.define(version: 20160717123817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20160625165050) do
   end
 
   add_index "auctions", ["product_id"], name: "index_auctions_on_product_id", using: :btree
+
+  create_table "auto_bids", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "auction_id"
+    t.integer "max_bid"
+  end
+
+  add_index "auto_bids", ["auction_id"], name: "index_auto_bids_on_auction_id", using: :btree
+  add_index "auto_bids", ["user_id"], name: "index_auto_bids_on_user_id", using: :btree
 
   create_table "bids", force: :cascade do |t|
     t.integer  "user_id"
@@ -71,6 +80,8 @@ ActiveRecord::Schema.define(version: 20160625165050) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "auctions", "products"
+  add_foreign_key "auto_bids", "auctions"
+  add_foreign_key "auto_bids", "users"
   add_foreign_key "bids", "auctions"
   add_foreign_key "bids", "users"
 end
