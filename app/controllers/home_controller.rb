979cae8ab4e760451ddf0f20c781908bc1a3237f
[a621275] Ibrahima khoule <ibrahima.khoule@atos.net>
2016-07-31 17:14:56 +0000

    infinite scroll

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
     def index
-            @auctionOnline= Auction.joins(:product).where("auctions.auction_close > ?", Time.now)
+        @auctionOnline = Auction.joins(:product).where("auctions.auction_close > ?", Time.now)
+        @auctionClosed  = Auction.joins(:product).where("auctions.auction_close < ?", Time.now)
     end
 end
