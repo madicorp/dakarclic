d41329555c5ff6d06051b0c72cc4de78eaaec403
[a621275] Ibrahima khoule <ibrahima.khoule@atos.net>
2016-08-04 20:47:23 +0000

    refonte home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 
     def index
-        @auctionOnline = Auction.joins(:product).where("auctions.auction_close > ?", Time.now).limit(4).order('created_at DESC')
-        @auctionClosed  = Auction.joins(:product).where("auctions.auction_close < ?", Time.now).limit(4).order('created_at DESC')
+        @auctionOnline = Auction.joins(:product).where("auctions.auction_close > ? and auctions.status = ? ", Time.now,true).limit(10).order('created_at DESC')
+        @auctionClosed  = Auction.joins(:product).where("auctions.auction_close < ?", Time.now).limit(10).order('created_at DESC')
+        @auctionComing = Auction.joins(:product).where("auctions.auction_close > ? and auctions.status = ? ", Time.now,false).limit(10).order('created_at DESC')
+
     end
 end
