5dada5c66a82261c5ac1eb7d83c93094a0aa60a8
Sega Diop <contact@madi-corp.net>
2016-08-05 09:53:05 +0000

    modifications on media validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,7 @@
 class Product < ActiveRecord::Base
     has_many :auction
     has_attached_file :image
-    validates_attachment :image,
-                         content_type: { content_type: ["image/jpeg", "image/jpg","image/gif", "image/png"] }
+    validates_attachment_content_type :image, :content_type => [/\Aimage/, 'application/octet-stream']
     def has_auction?
         auction.present?
     end
