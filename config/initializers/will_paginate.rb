4e6662ece27800689b5516c711d76dd426932be7
[a621275] Ibrahima khoule <ibrahima.khoule@atos.net>
2016-08-08 18:15:28 +0000

    add username

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,1 +1,1 @@
-WillPaginate.per_page = 8
+WillPaginate.per_page = 10
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804193336) do
+ActiveRecord::Schema.define(version: 20160808172720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,6 +121,7 @@ ActiveRecord::Schema.define(version: 20160804193336) do
     t.boolean  "admin"
     t.integer  "units"
     t.string   "avatar"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
